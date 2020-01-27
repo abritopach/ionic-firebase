5dfd2c7d78a4794678ea495c7eb2348f1ae62afa
abritopach <adrbrpa1988@gmail.com>
2020-01-27 21:51:38 +0100

    Added authentication component & added user model & added shared auth module

---
+++ src/app/components/auth-form/auth-form.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { UserCredential } from 'src/app/models/user';
+import { LoadingController, AlertController } from '@ionic/angular';
+
+@Component({
+    selector: 'app-auth-form',
+    templateUrl: './auth-form.component.html',
+    styleUrls: ['./auth-form.component.scss'],
+})
+export class AuthFormComponent implements OnInit {
+
+    // Our global loading controller in this class, it will trigger and hide the loading spinner.
+    public loading: HTMLIonLoadingElement;
+    // Will hold our authentication form.
+    public authForm: FormGroup;
+    // Is an input, meaning we'll pass it from the parent component, and this will set the text in our action button.
+    @Input() actionButtonText: string;
+    // Is a boolean that lets our form know if we're in the reset password page or not, we'll mainly use
+    // it to hide our password field when we need to use the password reset page.
+    @Input() isPasswordResetPage = false;
+    // Is an Output, and it's the value we'll emit from this component to the parent page.
+    @Output() formSubmitted = new EventEmitter<any>();
+
+    constructor(private formBuilder: FormBuilder, private loadingCtrl: LoadingController,
+                private alertCtrl: AlertController) {
+        this.authForm = this.formBuilder.group({
+            email: ['', Validators.compose([Validators.required, Validators.email])],
+            password: ['', Validators.minLength(6)]
+        });
+    }
+
+    ngOnInit() {}
+
+    async showLoading(): Promise<void> {
+        try {
+            await this.loading.present();
+            this.loading = await this.loadingCtrl.create();
+            await this.loading.present();
+        } catch (error) {
+            this.handleError(error);
+        }
+    }
+
+    hideLoading(): Promise<boolean> {
+        return this.loading.dismiss();
+    }
+
+    async handleError(error): Promise<void> {
+        const alert = await this.alertCtrl.create({
+            message: error.message,
+            buttons: [{ text: 'Ok', role: 'cancel' }]
+        });
+        await alert.present();
+    }
+
+    submitCredentials(authForm: FormGroup): void {
+        if (!authForm.valid) {
+            console.log('Form is not valid yet, current value:', authForm.value);
+        } else {
+            this.showLoading();
+            const credentials: UserCredential = {
+                email: authForm.value.email,
+                password: authForm.value.password
+            };
+            this.formSubmitted.emit(credentials);
+        }
+    }
+
+}
---
+++ src/app/shared-modules/auth.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { AuthFormComponent } from 'src/app/components/auth-form/auth-form.component';
+
+@NgModule({
+  declarations: [AuthFormComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ReactiveFormsModule
+  ],
+  exports: [AuthFormComponent],
+  entryComponents: [AuthFormComponent]
+})
+export class AuthModule { }
