a5325053bae37b66b5e0db537fff948a30ed44d8
abritopach <adrbrpa1988@gmail.com>
2020-02-11 21:28:37 +0100

    Added split pane

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,32 @@ import * as firebase from 'firebase/app';
 import { environment } from 'src/environments/environment';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform
-  ) {
-    this.initializeApp();
-  }
 
-  initializeApp() {
-    firebase.initializeApp(environment.firebaseConfig);
-    this.platform.ready().then(() => {
-    });
-  }
+    public appPages = [
+        {
+            title: 'Home',
+            url: '/home',
+            icon: 'home'
+        },
+        {
+            title: 'Events',
+            url: '/event-list',
+            icon: 'list-outline'
+        }
+    ];
+
+    constructor(private platform: Platform) {
+        this.initializeApp();
+    }
+
+    initializeApp() {
+        firebase.initializeApp(environment.firebaseConfig);
+            this.platform.ready().then(() => {
+        });
+    }
 }
